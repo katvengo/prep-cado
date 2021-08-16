2623298f7f80c6d18d25804c24ccb90e2b0cd69e
katvengo <katvengo@gmail.com>
2021-08-16 16:28:49 -0700

    added amplify node modules

--- .history/App_19851026011459.js
+++ .history/App_19851026011459.js
@@ -1,9 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Amplify from 'aws-amplify'
-import config from './src/aws-exports'
-Amplify.configure(config)
 
 export default function App() {
   return (
